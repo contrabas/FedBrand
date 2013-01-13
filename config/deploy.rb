4a46f1f0229b228974345f1df10393ee915bfdd8
Ravwar <rav_war@mail.ru>
2013-01-14 00:33:38 +0400

    Add deploy

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+require 'bundler/capistrano'
+
+server '178.63.96.144', :app, :web, :db, primary: true
+
+set :user, 'fedbrand'
+set :deploy_to, '/var/rails/fedbrand'
+
+set :use_sudo, false
+set :deploy_via, :remote_cache
+
+set :scm, :git
+set :repository, 'git@github.com:RavWar/FedBrand.git'
+set :normalize_asset_timestamps, false
+set :shared_children, shared_children + %w[public/wp-content]
+set :shared_children, shared_children + %w[public/system]
+set :shared_children, shared_children + %w[public/uploads]
+
+ssh_options[:forward_agent] = true
+
+namespace :deploy do
+  task(:start) {}
+  task(:stop)  {}
+
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
