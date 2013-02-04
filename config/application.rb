3b6e4e5a3526d445bf6fa34e2f64b3c5f888b917
Ravwar <rav_war@mail.ru>
2013-02-04 21:40:45 +0400

    Fixes for production

--- config/application.rb
+++ config/application.rb
@@ -60,8 +60,5 @@ module FedBrand
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
-
   end
 end
