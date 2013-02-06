b21583e611bbfe99d3677a452e1474e9c1c53030
Ravwar <rav_war@mail.ru>
2013-02-07 00:03:56 +0400

    Fix localeapp config for production

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,7 +1,14 @@
 require 'localeapp/rails'
 
 Localeapp.configure do |config|
-  config.polling_environments = [:development]
-  config.sending_environments = [:development]
+  # allow localeapp to check for new translations in production
+  config.polling_environments << :production
+  # the number of seconds to wait between checking for new translations
+  # enter `0` to check on every request
+  config.poll_interval = 60
+  # when new translations have been downloaded, reload I18n
+  config.reloading_environments << :production
+  # send missing translations to Locale in production
+  config.sending_environments << :production
   config.api_key = '21MkML5TBAKxvtIecUVwgVikYJJ0OfRQpdpFubdTie6ltQG8UT'
 end
