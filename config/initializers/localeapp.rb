3b332285dd24ea96d37855aad98c976b7ddaa681
Ravwar <rav_war@mail.ru>
2013-02-07 00:24:34 +0400

    Fix localeapp config, fix news content html display

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -5,10 +5,11 @@ Localeapp.configure do |config|
   config.polling_environments << :production
   # the number of seconds to wait between checking for new translations
   # enter `0` to check on every request
-  config.poll_interval = 60
+  config.poll_interval = 600
   # when new translations have been downloaded, reload I18n
   config.reloading_environments << :production
-  # send missing translations to Locale in production
-  config.sending_environments << :production
+  # do not send missing translations to Locale
+  config.sending_environments = []
+
   config.api_key = '21MkML5TBAKxvtIecUVwgVikYJJ0OfRQpdpFubdTie6ltQG8UT'
 end
