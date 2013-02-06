980c4be4d54ce393966d46d0fafecb67fc5eccab
Ravwar <rav_war@mail.ru>
2013-02-06 20:18:06 +0400

    Add some config options for localeapp

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,5 +1,7 @@
 require 'localeapp/rails'
 
 Localeapp.configure do |config|
+  config.polling_environments = [:development]
+  config.sending_environments = [:development]
   config.api_key = '21MkML5TBAKxvtIecUVwgVikYJJ0OfRQpdpFubdTie6ltQG8UT'
 end
