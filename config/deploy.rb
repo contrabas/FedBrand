f7a223a12b88ffc61429d45129e6cb9b9f33a0ad
Ravwar <rav_war@mail.ru>
2013-02-07 13:34:59 +0400

    Switch from polling in production in localeapp to daemon

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,19 @@ set :shared_children, shared_children + %w[public/uploads]
 ssh_options[:forward_agent] = true
 
 namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+end
+
+namespace :deploy do
   task(:start) {}
   task(:stop)  {}
 
--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,15 +1,9 @@
 require 'localeapp/rails'
 
 Localeapp.configure do |config|
-  # allow localeapp to check for new translations in production
-  config.polling_environments << :production
-  # the number of seconds to wait between checking for new translations
-  # enter `0` to check on every request
+  config.polling_environments = [:development]
   config.poll_interval = 600
-  # when new translations have been downloaded, reload I18n
   config.reloading_environments << :production
-  # do not send missing translations to Locale
   config.sending_environments = []
-
   config.api_key = '21MkML5TBAKxvtIecUVwgVikYJJ0OfRQpdpFubdTie6ltQG8UT'
 end
